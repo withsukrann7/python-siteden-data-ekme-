c200baef97bada4f425c3b15ead3d1457b199f68
withsukrann7 <sukran1903yilmaz@gmail.com>
2023-05-04 02:02:21 +0300

    fiyat adlı bilgi

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-"""
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://www.vatanbilgisayar.com/cep-telefonu-modelleri/'
-
-def check_price():
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    page = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(page.content,'html.parser')
-
-    # fiyatı sayfadan çek!
-
-    price = soup.find(id = 'list__price')
-
-    if price is not None:
-        converted_price = float(price.get_text()[1:])
-        print(f"ürün fiyatı: {converted_price}")
-    else:
-        print("fiyat bilgisi mevcut değildir...")
-    
-
-check_price
-"""
 
 import requests
 from bs4 import BeautifulSoup
